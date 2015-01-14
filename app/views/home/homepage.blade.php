78d2eb58dcdc984c429ecea29bed26bf861b1049
Alexandre <alexandre.capellaro@infomaniak.ch>
2015-01-14 07:53:25 +0100

    Enlever la seconde image du slider

--- app/views/home/homepage.blade.php
+++ app/views/home/homepage.blade.php
@@ -10,19 +10,12 @@
     <ol class="carousel-indicators">
         <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
         <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
     </ol>
 
 
     <div class="carousel-inner" role="listbox">
         <div class="item active">
             <img src="{{ URL::asset('images/header.png') }}" class="img-responsive brightness" data-holder-rendered="true">
-            <div class="carousel-caption">
-                Caveau Quinard, Viticulteur Récoltant
-            </div>
-        </div>
-        <div class="item">
-            <img src="{{ URL::asset('images/banniere.jpg') }}" class="img-responsive brightness" data-holder-rendered="true">
             <div class="carousel-caption">
                 Caveau Quinard, Viticulteur Récoltant
             </div>
