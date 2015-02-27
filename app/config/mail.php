0f86e39bd498e0cf3fe02f620b8b7c95a7f8d73d
Alexandre <alexandre.capellaro@infomaniak.ch>
2015-02-27 10:00:41 +0100

    config mail smtp prod

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.gmail.com',
+	'host' => 'ns0.ovh.net',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return array(
 	|
 	*/
 
-	'port' => 587,
+	'port' => 465,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'no-reply@caveauquinard.com', 'name' => 'Caveau Quinard'),
+	'from' => array('address' => 'no-reply@caveauquinard.fr', 'name' => 'Caveau Quinard'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return array(
 	|
 	*/
 
-	'encryption' => 'tls',
+	'encryption' => 'ssl',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'alexandre.capellaro@gmail.com',
+	'username' => 'postmaster@caveauquinard.fr',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'aex0108alex',
+	'password' => 'caveau01',
 
 	/*
 	|--------------------------------------------------------------------------
